4ca1dda3a0b5e5a1d00e1c43a0183ba62d0a044e
msrod <76046889+msrod@users.noreply.github.com>
2026-01-28 13:47:04 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -6,8 +6,17 @@ const FILES = [
 ];
 
 self.addEventListener("install", e => {
+ self.skipWaiting();
  e.waitUntil(
   caches.open(CACHE).then(c => c.addAll(FILES))
+ );
+});
+
+self.addEventListener("activate", e => {
+ e.waitUntil(
+  caches.keys().then(keys =>
+   Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+  )
  );
 });
 
@@ -15,5 +24,4 @@ self.addEventListener("fetch", e => {
  e.respondWith(
   caches.match(e.request).then(r => r || fetch(e.request))
  );
-
 });
